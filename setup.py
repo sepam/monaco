fff3cd3f7ba46f94c48f58e4bd13cbf59e3b46c0
sepam <mwieriks@gmail.com>
2020-03-29 21:22:00 +0700

    fixed entry_points typo

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='monaco',
-    version='0.1.1',
+    version='0.1.2',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
-    entry_point={
+    entry_points={
         'console_scripts': [
             'monaco=monaco.cli:main'
         ]
