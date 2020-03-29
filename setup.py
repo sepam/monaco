d9e28ab01105c6a1fdb5033709b1743811c30ce4
Martijn Wieriks <mwieriks@gmail.com>
2020-03-29 21:12:42 +0700

    add base requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='monaco',
-    packages=find_packages(where='src'),
+    version='0.1.1',
     package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    entry_point={
+        'console_scripts': [
+            'monaco=monaco.cli:main'
+        ]
+    }
 )
