40da9d1ec0b5d7f67d10ca131699beca667d04c7
sepam <mwieriks@gmail.com>
2020-03-29 21:26:00 +0700

    bumped version number in cli app

--- src/monaco/cli.py
+++ src/monaco/cli.py
@@ -6,10 +6,10 @@ import click
 
 # @click.version_option(version=__version__)
 @click.command()
-@click.version_option(version="0.1.1")
+@click.version_option(version="0.1.2")
 def main():
     """The Monaco project."""
-    click.secho("Welcome to Monaco -- an easier way to plan projects", fg='green')
+    click.secho("Welcome to Monaco - an easier way to plan projects", fg='green')
 
 
 if __name__ == '__main__':
